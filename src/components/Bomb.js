9ae124b6dd24f6c7b1e2143d9993c5655014f73e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 14:25:38 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {secondsLeft: this.props.}
+  }
   render() {
     return (
       //code
