69fd3ac21f18b1b0479db17b943c14f1654a141c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 14:28:52 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -3,11 +3,17 @@ import React from 'react';
 export default class Bomb extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {secondsLeft: this.props.}
+    this.state = {secondsLeft: props.initialCount}
   }
   render() {
     return (
-      //code
+      <div>
+      if (this.state.secondsLeft > 0) {
+        {this.state.secondsLeft} seconds left before I go boom!
+      } else {
+        Boom!
+      }
+      </div>
     );
   }
 }
